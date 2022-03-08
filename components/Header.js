cc557a3e5e505236a331bb6a939de1a811242214
ugochukwu <ugochukwuaugustine299@gmail.com>
2022-03-07 21:51:27 -0700

    Configuring, Styling and TextInput

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import Colors from '../constants/colors';
+
 const Header = props => {
   return (
     <View style={styles.header}>
@@ -8,16 +10,16 @@ const Header = props => {
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   header: {
     width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: '#f7287b',
+    backgroundColor: Colors.primary,
     alignItems: 'center',
     justifyContent: 'center'
   },
-  
   headerTitle: {
     color: 'black',
     fontSize: 18
